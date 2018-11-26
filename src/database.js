5a1a2c7c4984a79fb5d858fa30fda5d36b596b56
Aandresmc <llaandresmcll@gmail.com>
2018-11-25 19:36:01 -0500

    prueba de resubida

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost/node-notes-db', {
+mongoose.connect('mongodb://Aandresmc:andres14@ds153198.mlab.com:53198/node-notes-db', {
+
   useCreateIndex: true,
   useNewUrlParser: true
 })
